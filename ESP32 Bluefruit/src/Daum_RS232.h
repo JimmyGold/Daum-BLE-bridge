faa0900c4cdfa951c727b20bb92391d767b96cb7
JimmyGold <82078038+JimmyGold@users.noreply.github.com>
2021-05-12 07:26:50 +0200

    Update Daum_RS232.h

--- ESP32 Bluefruit/src/Daum_RS232.h
+++ ESP32 Bluefruit/src/Daum_RS232.h
@@ -3,5 +3,5 @@
 void DaumRequestData(void);
 
 void DaumReceiveData(bool *connected, uint8_t *program, uint8_t *person, bool *pedaling, uint16_t *powerPreset, 
-                        uint8_t *speed, uint8_t *cadence, float *distance, uint16_t *time, float *kJoule, uint8_t *pulse, uint8_t *pulseStatus,
+                        uint16_t *speed, uint8_t *cadence, float *distance, uint16_t *time, float *kJoule, uint8_t *pulse, uint8_t *pulseStatus,
                         uint8_t *gear,  uint16_t *power);
